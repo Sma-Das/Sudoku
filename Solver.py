e1f3960c921f4500254b9c49336502d420f1987a
Sma-Das <smaddas10@gmail.com>
2021-01-19 12:03:42 +0400

    Added solver class

--- Solver.py
+++ Solver.py
@@ -7,3 +7,7 @@ class Solver:
 
     def options(self, row: int, column: int) -> set:
         valid = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        valid -= self.puzzle.row(row)
+        valid -= self.puzzle.column(column)
+        valid -= self.puzzle.block(row, column)
+        return valid
